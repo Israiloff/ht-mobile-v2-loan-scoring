d6d32ab134173f586c7cca60c1becebfd39f1642
israiloff <stardustindark@gmail.com>
2025-07-31 17:09:10 +0500

    fixed: camunda request members;

--- src/main/java/uz/hayotbank/camundarest/model/scoring/CLoanScoringRequest.java
+++ src/main/java/uz/hayotbank/camundarest/model/scoring/CLoanScoringRequest.java
@@ -2,11 +2,13 @@ package uz.hayotbank.camundarest.model.scoring;
 
 import java.io.Serializable;
 import java.util.UUID;
+import uz.cbssolutions.commons.loan.obtain.models.camunda.FaceId;
 
 /**
  * Camunda scoring request model.
  *
  * @param applicationId Loan application ID.
+ * @param faceId Face ID object containing the URL and bucket.
  */
-public record CLoanScoringRequest(UUID applicationId) implements Serializable {
+public record CLoanScoringRequest(UUID applicationId, FaceId faceId) implements Serializable {
 }
