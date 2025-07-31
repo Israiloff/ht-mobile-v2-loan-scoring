f9a0385a97746caca7e625f2e54e72921735e964
israiloff <stardustindark@gmail.com>
2025-07-31 17:19:01 +0500

    fixed: javadocs;

--- src/main/java/uz/hayotbank/loanscoring/mapper/CLoanScoringRequestMapper.java
+++ src/main/java/uz/hayotbank/loanscoring/mapper/CLoanScoringRequestMapper.java
@@ -5,12 +5,16 @@ import org.mapstruct.Mapper;
 import uz.cbssolutions.commons.loan.obtain.models.camunda.FaceId;
 import uz.hayotbank.camundarest.model.scoring.CLoanScoringRequest;
 
+/**
+ * Mapper interface for converting data to {@link CLoanScoringRequest}.
+ *
+ */
 @Mapper(componentModel = "spring")
 public interface CLoanScoringRequestMapper {
 
     /**
      * Maps the given arguments to a {@link CLoanScoringRequest}.
-     * 
+     *
      * @param applicationId The ID of the loan application.
      * @param faceId The FaceId object containing the URL and bucket.
      * @return A {@link CLoanScoringRequest} instance containing the application ID and FaceId.
